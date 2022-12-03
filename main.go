2e904f8ad01a2fb11dc297aa52027dd61cb5a0a2
Guillermo Ruiz <guillermoruizbaires@gmail.com>
2022-12-03 09:56:05 -0600

    Read keyboard input

--- main.go
+++ main.go
@@ -2,50 +2,21 @@ package main
 
 import "fmt"
 
-/*
-	go build main.go -> an excecutable file
- 	go run  main.go -> this gone excecute file internaly
-*/
-// this can be more simple
-// const Sunday int = 0
-// const Monday int = 1
-// const Tuesday int = 2
-// const Wednesday = 3
-// const Thursday = 4
-// const Friday = 5
-// const Saturday = 6
+// Read the Keyboard input with fmt.Scan
 
-// const (
-// 	Sunday    int = 0
-// 	Monday    int = 1
-// 	Tuesday   int = 2
-// 	Wednesday     = 3
-// 	Thursday      = 4
-// 	Friday        = 5
-// 	Saturday      = 6
-// )
-// but can be even better
-const (
-	Sunday int = iota // the default value is "0"
-	Monday
-	Tuesday
-	Wednesday
-	Thursday
-	Friday
-	Saturday
-) // this kind of code is a sequence value
+var (
+	name   string
+	age    int
+	height float32
+)
 
 func main() {
-	// fmt.Println(Saturday)
-	// fmt.Println(Sunday)
-	// fmt.Println(Thursday)
-
-	textString := "Hi!, This is a string"
-	firstCharacter := textString[0]
-	lastCharacter := textString[len(textString)-1]
-
-	fmt.Println(textString[0]) // --> this is because this type is uint8
-	fmt.Printf("%c\n", firstCharacter)
-	fmt.Printf("%c", lastCharacter)
+	fmt.Print("Please write your name here: ")
+	fmt.Scanf("%s", &name)
+	fmt.Print("how old are you? ")
+	fmt.Scanf("%d", &age)
+	fmt.Print("Write your height: ")
+	fmt.Scanf("%f", &height)
+	fmt.Printf("Hi mi name is %s i'm %d years old, and my height is %.2f", name, age, height)
 
 }
