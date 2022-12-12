7cc431f1f95ca6790fa031fc0147e00c96c695dd
Guillermo Ruiz <guillermoruizbaires@gmail.com>
2022-12-12 11:23:27 -0600

    slicing slices

--- main.go
+++ main.go
@@ -1,22 +1,32 @@
 package main
 
-import (
-	"fmt"
-)
+import "fmt"
 
-// **** Understanding capacity **** //
+// **** Understanding slices **** //
 
 func main() {
-	var x []int
-	fmt.Println(x, len(x), cap(x))
-	x = append(x, 10)
-	fmt.Println(x, len(x), cap(x))
-	x = append(x, 20)
-	fmt.Println(len(x), cap(x))
-	x = append(x, 30)
-	fmt.Println(x, len(x), cap(x))
-	x = append(x, 40)
-	fmt.Println(x, len(x), cap(x))
-	x = append(x, 50)
-	fmt.Println(x, len(x), cap(x))
+
+	// Slicing slices
+
+	x := []int{1, 2, 3, 4}
+	y := x[:2]
+	z := x[1:]
+	d := x[1:3]
+	e := x[:]
+
+	fmt.Println("x:", x)
+	fmt.Println("y:", y)
+	fmt.Println("z:", z)
+	fmt.Println("d:", d)
+	fmt.Println("e:", e)
+
+	fmt.Println("------------------")
+	fmt.Println("Slices with overlapping storage:")
+	x[1] = 20
+	y[0] = 10
+	z[1] = 30
+	fmt.Println("x:", x)
+	fmt.Println("y:", y)
+	fmt.Println("z:", z)
+
 }
