416a38f44ca84dc8c41b30f04972c497e6202468
Guillermo Ruiz <guillermoruizbaires@gmail.com>
2022-12-28 15:26:29 -0600

    Pointers
    Delete function.go, use a pointer example

--- main.go
+++ main.go
@@ -4,22 +4,39 @@ import (
 	"fmt"
 )
 
-func accumulator(increment int) func() int {
-	i := 0
-	return func() int {
-		i = i + increment
-		return i
-	}
-}
+// func accumulator(increment int) func() int {
+// 	i := 0
+// 	return func() int {
+// 		i = i + increment
+// 		return i
+// 	}
+// }
 
 // **** Function closure.
 
-func main() {
-	a := accumulator(1)
-	b := accumulator(2)
+func a(i int) {
+	i = 0
+}
 
-	fmt.Println("a", "b")
-	for i := 0; i < 5; i++ {
-		fmt.Println(a(), b())
-	}
+func b(i *int) {
+	*i = 0
+}
+
+func main() {
+	// 	a := accumulator(1)
+	// 	b := accumulator(2)
+
+	// 	fmt.Println("a", "b")
+	// 	for i := 0; i < 5; i++ {
+	// 		fmt.Println(a(), b())
+	// 	}
+	x := 100
+	a(x)
+	fmt.Println(x)
+	b(&x)
+
+	fmt.Println(x)
+
+	fmt.Println(&x)
+
 }
